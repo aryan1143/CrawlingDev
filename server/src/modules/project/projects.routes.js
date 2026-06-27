43e7c44e855f7e85e6b206f2f37f54673372be8a
Aryan <aryanshasap@gmail.com>
2026-06-27 17:26:08 +0530

    feat: fixed wrong controller in /projects delete route

--- server/src/modules/project/projects.routes.js
+++ server/src/modules/project/projects.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProject } from "./projects.controller.js";
+import { createProject, deleteProject } from "./projects.controller.js";
 import { requireAuth } from "../../shared/jwt.js";
 import { upload } from "../../config/cloudinary.js";
 
@@ -9,6 +9,6 @@ import { upload } from "../../config/cloudinary.js";
 const router = Router();
 
 router.post("/", upload.array("images", 3), createProject);
-router.delete("/:projectId", createProject);
+router.delete("/:projectId", deleteProject);
 
 export default router;
